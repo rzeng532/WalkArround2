732c0ec0639deb2b5c6d131876c9edef6c0e17ab
mashanshanshan <mashan0537@126.com>
2018-05-30 23:26:44 +0800

    会话页面删除按钮位置微调整

--- app/src/main/java/com/awalk/walkarround/message/adapter/BaseConversationListAdapter.java
+++ app/src/main/java/com/awalk/walkarround/message/adapter/BaseConversationListAdapter.java
@@ -195,7 +195,7 @@ public class BaseConversationListAdapter extends BaseAdapter implements OnClickL
             holder.rlConversation = (RelativeLayout) convertView.findViewById(R.id.conv_rl);
             holder.rlFilfullArea = (RelativeLayout) convertView.findViewById(R.id.filfull_area);
             holder.ivPortrait = (PhotoView) convertView.findViewById(R.id.conv_portrait);
-            holder.ivDelIcon = (RelativeLayout) convertView.findViewById(R.id.conversation_item_del_icon);
+            holder.ivDelIcon = convertView.findViewById(R.id.conversation_item_del_icon);
             //holder.ivPortrait.setCheckBoxResId(R.drawable.public_icon_list_checkbox_on,
             //        R.drawable.public_icon_list_checkbox_off);
             //holder.ivPortrait.setCheckBoxClickable(false);
@@ -640,7 +640,7 @@ public class BaseConversationListAdapter extends BaseAdapter implements OnClickL
         RelativeLayout rlConversation;
         RelativeLayout rlFilfullArea;
         ImageView ivTopSign;
-        RelativeLayout ivDelIcon;
+        View ivDelIcon;
         TextView tvName;
         TextView tvMessage;
         TextView tvTime;
