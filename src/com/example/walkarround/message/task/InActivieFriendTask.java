6b1bb860d553d3c9fcd57101f6cf1d9ebec3736b
mashanshanhz <38494533+mashanshanhz@users.noreply.github.com>
2018-12-17 15:50:02 +0800

    Update InActivieFriendTask.java

--- src/com/example/walkarround/message/task/InActivieFriendTask.java
+++ src/com/example/walkarround/message/task/InActivieFriendTask.java
@@ -9,7 +9,7 @@ import com.example.walkarround.util.http.HttpUtil;
 import java.util.Map;
 
 /**
- * Created by cmcc on 16/1/26.
+ * Created on 16/1/26.
  */
 public class InActivieFriendTask extends HttpTaskPost {
     public InActivieFriendTask(Context context, onResultListener listener, String requestCode, String urlString, String contentStr, Map<String, String> header) {
