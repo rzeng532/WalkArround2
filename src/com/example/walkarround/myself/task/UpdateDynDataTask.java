4e2a7e3a3a4ed9abd0f63c6f383bade474f45968
mashanshanhz <38494533+mashanshanhz@users.noreply.github.com>
2018-12-17 15:51:39 +0800

    Update UpdateDynDataTask.java

--- src/com/example/walkarround/myself/task/UpdateDynDataTask.java
+++ src/com/example/walkarround/myself/task/UpdateDynDataTask.java
@@ -9,7 +9,7 @@ import com.example.walkarround.util.http.HttpUtil;
 import java.util.Map;
 
 /**
- * Created by cmcc on 16/1/26.
+ * Created on 16/1/26.
  */
 public class UpdateDynDataTask extends HttpTaskPost {
     public UpdateDynDataTask(Context context, onResultListener listener, String requestCode, String urlString, String contentStr, Map<String, String> header) {
